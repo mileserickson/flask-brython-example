a3972f456f58f2605df3128a206ec115740f17ed
Miles Erickson <miles.erickson@gmail.com>
2018-11-12 15:31:02 -0800

    Add spam model

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import random
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+import pickle
 from flask import Flask, request, render_template, jsonify
 
 
+with open('spam_model.pkl') as f:
+    model = pickle.load(f)
 app = Flask(__name__, static_url_path="")
-
 
 @app.route('/')
 def index():
@@ -14,5 +20,6 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     """Return a random prediction."""
+    data = request.json
     return jsonify({'probability': random.random()})
 
