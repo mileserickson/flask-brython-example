07109d1e1439de6c6e30aeb8c20166d283148d8c
Miles Erickson <miles.erickson@gmail.com>
2018-11-12 15:54:03 -0800

    Connect spam classifier to predict method

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pickle
 from flask import Flask, request, render_template, jsonify
 
 
-with open('spam_model.pkl') as f:
+with open('spam_model.pkl', 'rb') as f:
     model = pickle.load(f)
 app = Flask(__name__, static_url_path="")
 
@@ -21,5 +21,6 @@ def index():
 def predict():
     """Return a random prediction."""
     data = request.json
-    return jsonify({'probability': random.random()})
+    prediction = model.predict_proba([data['user_input']])
+    return jsonify({'probability': prediction[0][1]})
 
